20afdfdde3ff52616ca3ee03f591795ff86e4d6f
Nick <nikitalytvynov0506@gmail.com>
2023-08-22 15:42:18 +0300

    sii

---
+++ src/ProfileCard.js
@@ -0,0 +1,19 @@
+function ProfileCard({title, handle}) {
+    // const title = props.title;
+    // const handle = props.handle;
+
+    //const { title, handle } = props;
+
+    return (
+        <div>
+            <div>
+                Title is {title}
+            </div>
+            <div>
+                Handle is {handle}
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
