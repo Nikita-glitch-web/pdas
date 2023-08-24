3dd2f0bb57edce012f7682241fbb6d11107efd08
Nick <nikitalytvynov0506@gmail.com>
2023-08-24 16:58:23 +0300

    sii

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,20 +1,28 @@
-function ProfileCard({title, handle}) {
+// this is child component
+
+function ProfileCard({title, handle, image, description}) {
     // const title = props.title;
     // const handle = props.handle;
 
     //const { title, handle } = props;
-
+    console.log(handle)
     return (
-        <div>
-            <div>
-                Title is {title}
-                SUCKKK
-            </div>
-            <div>
-                Handle is {handle}
-                SUCK
-            </div>
+      <div className="card">
+        <div className="crad-image">
+          <figure className="image is-1by1">
+            <img src={image} alt="pda logo" />
+          </figure>
         </div>
+        <div className="card-content">
+          <div className="median-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
+          <div className="content">
+            {description}
+          </div>
+        </div>
+      </div>
     );
 }
 
